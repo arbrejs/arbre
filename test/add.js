0c0a5ea20b241fe25a3e1c0e523407f703883bac
ngryman <ngryman@gmail.com>
2016-09-08 22:35:39 +0100

    moar tests

--- test/add.js
+++ test/add.js
@@ -1,6 +1,13 @@
 import test from 'ava'
 import grapes, { Node } from '../'
 
+test('return the new child', t => {
+  const tree = grapes()
+  const node = tree.add(1)
+
+  t.is(tree.at(0), node)
+})
+
 test('add a node', t => {
   const tree = grapes()
   tree.add(grapes(1))
@@ -17,9 +24,19 @@ test('add a value', t => {
   t.is(tree.at(0).value, 1)
 })
 
-test('return the new child', t => {
+test('move an existing node', t => {
   const tree = grapes()
-  const nodeRef = tree.add(1)
+  const node = tree.add(1).add(2)
+  tree.add(node)
 
-  t.is(tree.at(0), nodeRef)
+  t.is(tree.at(0).value, 1)
+  t.is(tree.at(1).value, 2)
+})
+
+test('do not add itself', t => {
+  const tree = grapes()
+  const node = tree.add(tree)
+
+  t.is(tree, node)
+  t.not(tree.parent, tree)
 })
--- test/insert-at.js
+++ test/insert-at.js
@@ -18,3 +18,24 @@ test('insert at the given index', t => {
   t.is(node.at(1).value.type, 'baz')
   t.is(node.at(2).value.type, 'qux')
 })
+
+test('ignore an non-existing parent', t => {
+  const node = grapes()
+  t.notThrows(() => node.insertAt())
+})
+
+test('ignore out of bounds indices', t => {
+  const tree = grapes()
+  const node = tree.add(1)
+
+  t.notThrows(() => node.insertAt(tree, -1))
+  t.notThrows(() => node.insertAt(tree, 1))
+})
+
+test('do not insert itself', t => {
+  const tree = grapes()
+  const node = tree.add(1)
+  node.insertAt(node, 0)
+
+  t.not(node.parent, node)
+})

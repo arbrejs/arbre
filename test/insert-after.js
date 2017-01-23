d935b848d925481f6dbfcca19dbf759091a159d1
ngryman <ngryman@gmail.com>
2017-01-23 12:24:00 +0100

    wip

--- test/insert-after.js
+++ test/insert-after.js
@@ -2,23 +2,23 @@ import test from 'ava'
 import insertAfter from '../lib/insert-after'
 
 test('insert after a node', t => {
-  const node = Tree('foo', 'bar', 'qux')
-  const after = node.children[0]
-  const inserted = Tree('baz')
-  const ret = insertAfter(inserted, after)
+  const parent = Tree('foo', 'bar', 'qux')
+  const node = parent.children[0]
+  const target = Tree('baz')
+  const ret = insertAfter(node, target)
 
-  t.is(ret, inserted)
+  t.is(ret, target)
   t.deepEqual(node, Tree('foo', 'bar', 'baz', 'qux'))
 })
 
 test('ignore orphan node', t => {
   const node = Tree()
-  const inserted = Tree()
-  const ret = insertAfter(inserted, node)
+  const target = Tree()
+  const ret = insertAfter(node, target)
 
   t.is(ret, undefined)
   t.is(node.children.length, 0)
-  t.not(inserted.parent, node)
+  t.not(target.parent, node)
 })
 
 test('ignore itself', t => {
